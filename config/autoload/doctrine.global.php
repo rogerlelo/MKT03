1ff11a7682c5b4fa05db1405ccd7f177d13c8095
Luiz <argentinaluiz@gmail.com>
2016-10-21 00:51:47 -0200

    Resolvendo erros na rota /teste

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -8,14 +8,26 @@ return [
                 'params'=>[
                     'host'=>'localhost',
                     'port'=>'3306',
-                    'user'=>'homestead',
-                    'password'=>'secret',
-                    'dbname'=>'codeemailmkt',
+                    'user'=>'root',
+                    'password'=>'root',
+                    'dbname'=>'code_php7',
                     'driverOptions'=>[
                         \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
                     ]
                 ]
             ]
-        ]
+        ],
+        'driver' => [
+            'CodeEmailMKT_driver' => [
+                'class' => 'Doctrine\ORM\Mapping\Driver\YamlDriver',
+                'cache' => 'array',
+                'paths' => [__DIR__ . '/../../src/CodeEmailMKT/Infrastructure/Persistence/Doctrine/ORM']
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    'CodeEmailMKT\Domain\Entity' => 'CodeEmailMKT_driver'
+                ]
+            ]
+        ],
     ]
 ];
